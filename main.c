99dd575355f571b81ce7370745f0100e1322a25e
Tyler McGinn <=>
2020-04-11 20:50:43 -0700

    testing bin file

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 int main(){
+    char* message = "hello world, this is a binary file!";
+    // char c;
+    // FILE* fp;
+    // fp = fopen("./data.bin", "wb");
+    int count = 0;
+    while(*message++ != '\0'){
+        count++;
     
+    }
+    printf("%d\n",count);
+    // printf("%d\n",strlen(message));
     system("pause");
     return 0;
 }
